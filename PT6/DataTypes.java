ed074b4ef920fdaccf37d77231a6c668a87aec8b
Radge Carbonel <84826548+rcarbon-del@users.noreply.github.com>
2023-09-01 19:05:42 +0800

    Add files via upload

---
+++ PT6/DataTypes.java
@@ -0,0 +1,22 @@
+/*Programmed by: <Write your name here>
+Title: DataTypes.java
+Date: <Write date here>*/
+
+public class DataTypes {
+    public static void main(String[] args) {
+    	String Lastname = "<Your Lastname>";
+    	String Firstname = "<Your Firstname>";
+    	char MiddleInitial = /*'<Your Middle Initial>'*/;
+    	int Age =  /*<Your Age>*/;
+    	String Course = "<Your Course>";
+    	int Year = /*<Your Year>*/;
+    	
+    	System.out.println("Student Information");
+    	System.out.println("Lastname: " + Lastname);
+    	System.out.println("Firstname: " + Firstname);
+    	System.out.println("MiddleInitial: " + MiddleInitial);
+    	System.out.println("Age: " + Age);
+    	System.out.println("Course: " + Course);
+    	System.out.println("Year: " + Year);
+    }
+}
