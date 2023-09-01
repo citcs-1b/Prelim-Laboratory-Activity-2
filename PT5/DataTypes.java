073627aef05a5755d3f9ce37bca18a8219198eb5
Radge Carbonel <84826548+rcarbon-del@users.noreply.github.com>
2023-09-01 18:43:49 +0800

    Add files via upload

---
+++ PT5/DataTypes.java
@@ -0,0 +1,18 @@
+/*Programmed by: <Write your name here>
+Title: DataTypes.java
+Date: <Write date here>*/
+
+public class DataTypes {
+    public static void main(String[] args) {
+    	char c = 'C';
+    	int i = 97;
+    	double d = 0.00;
+    	byte b = 0;
+    	i = b;
+    	String s = 'S';
+    	System.out.println("c = " + c);
+    	System.out.println("i = " + i);
+    	System.out.println("d = " + d);
+    	System.out.println("b = " + b);
+    }
+}
