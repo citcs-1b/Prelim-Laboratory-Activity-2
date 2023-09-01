45feacbda2f707741d8f9c5bdd9b9a1a76c6f308
Radge Carbonel <84826548+rcarbon-del@users.noreply.github.com>
2023-09-01 19:08:10 +0800

    Create Example.java

---
+++ PT6/Example.java
@@ -0,0 +1,22 @@
+/*Programmed by: <Write your name here>
+Title: DataTypes.java
+Date: <Write date here>*/
+
+public class Example {
+    public static void main(String[] args) {
+    	String Lastname = "Carbonel";
+    	String Firstname = "Radge Daryll";
+    	char MiddleInitial = 'A';
+    	int Age = 18;
+    	String Course = "Bachelor of Science in Computer Science";
+    	int Year = 1;
+    	
+    	System.out.println("Student Information");
+    	System.out.println("Lastname: " + Lastname);
+    	System.out.println("Firstname: " + Firstname);
+    	System.out.println("MiddleInitial: " + MiddleInitial);
+    	System.out.println("Age: " + Age);
+    	System.out.println("Course: " + Course);
+    	System.out.println("Year: " + Year);
+    }
+}
